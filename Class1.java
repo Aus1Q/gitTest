7f42b86c7e6718466104b57e968d4c53b31ddbf5
Austin Sanderson <asanderson2@stu.jsu.edu>
2022-09-01 18:21:08 -0500

    Initial Commit

---
+++ Class1.java
@@ -0,0 +1,9 @@
+package gitTest;
+
+public class Class1 {
+    
+    private void f1(){
+        // Placeholder Method (v1.0)
+    }
+    
+}
---
+++ Class2.java
@@ -0,0 +1,9 @@
+package gitTest;
+
+public class Class2 {
+
+    private void f1(){
+        // Placeholder Method (v1.0)
+    }
+    
+}
