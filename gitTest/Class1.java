c9419019f91594437ad4d3b5481f3d681ea2f837
Aus1Q <112362992+Aus1Q@users.noreply.github.com>
2022-09-01 18:44:15 -0500

    Add files via upload

---
+++ gitTest/Class1.java
@@ -0,0 +1,16 @@
+package gitTest;
+
+public class Class1 {
+    
+    private void f1(){
+        // Placeholder Method (v1.0)
+    }
+
+    private void f2(){
+        // Placeholder Method (v2.0)
+    }
+
+    private void f3(){
+        // Placeholder Method (v3.0)
+    }
+}
---
+++ gitTest/Class2.java
@@ -0,0 +1,17 @@
+package gitTest;
+
+public class Class2 {
+
+    private void f1(){
+        // Placeholder Method (v1.0)
+    }
+
+    private void f2(){
+        // Placeholder Method (v2.0)
+    }
+
+    private void f3(){
+        // Placeholder Method (v3.0)
+    }
+
+}
